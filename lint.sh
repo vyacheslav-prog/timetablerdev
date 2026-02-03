22360790e07bcb9a4e6df494f70e8cc3f5c64580
vyacheslav <vyacheslav.soynov@gmail.com>
2026-02-03 08:55:50 +0700

    feat: apply a flags for lint running

--- lint.sh
+++ lint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker run -t --rm -v $(pwd):/app -w /app golangci/golangci-lint:v2.5.0 golangci-lint run
+docker run -t --rm -v $(pwd):/app -w /app golangci/golangci-lint:v2.6.0 golangci-lint run $1
